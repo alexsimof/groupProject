33191720bff0fa1e36f1c48e6ab2e8b217d16d7e
Alex Simonov <alex.simonof@gmail.com>
2024-03-12 18:53:33 +0200

    finished header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 let btn = document.querySelector('.header-burger')
 let menu = document.querySelector('.header-burger-menu')
 let btnClose = document.querySelector('.header-burger-close')
+let dark = document.querySelector('.header-wrapper')
 btn.onclick = function (e) {
   e.preventDefault();
   menu.classList.add('show')
-  btn.classList.add('header-dark')
+  dark.classList.add('header-dark')
 }
 
 btnClose.onclick = function (e) {
   e.preventDefault();
   menu.classList.remove('show')
-  btn.classList.remove('header-dark')
+  dark.classList.remove('header-dark')
 }
